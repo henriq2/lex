1ff99c1b8f7590c0e66ff7fa615804d775da0fee
henriqcs <henrique@brainn.co>
2020-04-23 16:04:56 -0300

    new feature: reserved words

--- lex.c
+++ lex.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 
 #define INPUT_SIZE 80
+#define RWORDS 9
 
 typedef struct {
 	int size;
@@ -16,15 +17,78 @@ char tempS[INPUT_SIZE] = "";
 int cursor = 0;
 int action = 0;
 int tempN;
+char tempC;
+const char const * tabP[RWORDS] = {
+	"IF",
+	"THEN",
+	"ELSE",
+	"GOTO",
+	"LET",
+	"END",
+	"PRINT",
+	"READ",
+	"OF"
+};
 
 void act(void);
 void TabVar_ls(void);
+void machine(void);
+void tabP_ls(void);
 
 int main(void)
 {
-	printf("Input: ");
-	fgets(input, INPUT_SIZE, stdin);
+	while (fgets(input, INPUT_SIZE, stdin)) {
+		machine();
+	}
+	printf("\n");
+	TabVar_ls();
+	printf("\n");
+	tabP_ls();
+	return 0;
+}
+
+void machine()
+{
+	char c;
+	cursor = 0;
+	action = 0;
 	goto e0;
+
+e0:
+	act();
+	c = input[cursor];
+	if (c == '\0') {
+		return;
+	} else if (isalpha(c)) {
+		cursor++; // consome input
+		action = 1;
+		goto e1;
+	} else if (isspace(c) && c != '\n') {
+		cursor++;
+		action = 0;
+		goto e0;
+	} else if (isdigit(c)) {
+		cursor++;
+		action = 4;
+		goto e2;
+	} else if (c == '%') {
+		cursor++;
+		action = 0;
+		goto e6;
+	} else if (c == ':') {
+		cursor++;
+		action = 0;
+		goto e3;
+	} else if (ispunct(c)) {
+		cursor++;
+		action = 9;
+		goto e5;
+	} else if (c == '\n') {
+		printf("\n");
+		return;
+	} else {
+		goto rejeita;
+	}
 
 aceita:
 	printf("\nInput aceito\n");
@@ -35,29 +99,10 @@ rejeita:
 	printf("\nInput rejeitado\n");
 	exit(EXIT_FAILURE);
 
-e0:
-	act();
-	if (isalpha(input[cursor])) {
-		cursor++; //consome input
-		action = 1;
-		goto e1;
-	} else if (isspace(input[cursor]) && input[cursor] != '\n') {
-		cursor++;
-		action = 0;
-		goto e0;
-	} else if (isdigit(input[cursor])) {
-		cursor++;
-		action = 4;
-		goto e2;
-	} else if (input[cursor] == '\n') {
-		goto aceita;
-	} else {
-		goto rejeita;
-	}
-
 e1:
 	act();
-	if (isalnum(input[cursor])) {
+	c = input[cursor];
+	if (isalnum(c)) {
 		cursor++;
 		action = 2;
 		goto e1;
@@ -68,7 +113,8 @@ e1:
 
 e2:
 	act();
-	if (isdigit(input[cursor])) {
+	c = input[cursor];
+	if (isdigit(c)) {
 		cursor++;
 		action = 5;
 		goto e2;
@@ -77,6 +123,29 @@ e2:
 		goto e0;
 	}
 
+e3:
+	act();
+	c = input[cursor];
+	if (c == '=') {
+		cursor++;
+		action = 8;
+		goto e0;
+	} else {
+		action = 7;
+		goto e0;
+	}
+
+e5:
+	act();
+	action = 10;
+	goto e0;
+
+e6:
+	act();
+	while (input[cursor] != '\n' && input[cursor] != '\0')
+		cursor++;
+	action = 0;
+	goto e0;
 }
 
 int TabVar_entry(char *string)
@@ -100,11 +169,29 @@ void TabVar_ls()
 		printf("V(%d) = %s\n", i, tabVar.entries[i]);
 }
 
+void tabP_ls()
+{
+	for (int i = 0; i < RWORDS; i++)
+		printf("P(%d) = %s\n", i, tabP[i]);
+}
+
+int rword_entry(char *string)
+{
+	for (int i = 0; i < RWORDS; i++)
+		if (strcmp(tabP[i], string) == 0)
+			return i;
+	return -1;
+}
+
 void act()
 {
 	int entry;
-	char c = input[cursor - 1];
+	char c;
 	char *strend;
+
+	if (cursor > 0) {
+		c = input[cursor - 1];
+	}
 
 	switch (action) {
 	case 1:
@@ -117,10 +204,15 @@ void act()
 		*(strend + 1) = '\0';
 		break;
 	case 3:
-		entry = TabVar_entry(tempS);
-		if (entry == -1)
-			entry = TabVar_add_entry(tempS);
-		printf("V(%d)", entry);
+		entry = rword_entry(tempS);
+		if (entry != -1) {
+			printf("P(%d)", entry);
+		} else {
+			entry = TabVar_entry(tempS);
+			if (entry == -1)
+				entry = TabVar_add_entry(tempS);
+			printf("V(%d)", entry);
+		}
 		break;
 	case 4:
 		tempN = (c - '0');
@@ -131,6 +223,17 @@ void act()
 	case 6:
 		printf("N(%d)", tempN);
 		break;
+	case 7:
+		printf(":");
+		break;
+	case 8:
+		printf(":=");
+		break;
+	case 9:
+		tempC = c;
+		break;
+	case 10:
+		printf("%c", c);
+		break;
 	}
 }
-
